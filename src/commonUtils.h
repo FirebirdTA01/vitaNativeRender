8af5d7b727720dfebea31648b4e23ca1f83b4ef2
FirebirdTA01 <FirebirdTA01@aol.com>
2025-07-19 09:53:18 -0500

    Added Vector2f and TexturedVertex to commonUtils

--- src/commonUtils.h
+++ src/commonUtils.h
@@ -2,25 +2,19 @@
 
 #define PI 3.1415926535897932384626433832795028841971693993751f
 
-struct Color
+struct Vector2f
 {
-	Color() : r(0.0f), g(0.0f), b(0.0f), a(0.0f) {};
-	Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {};
-	float r, g, b, a;
-};
-
-struct ClearVertex
-{
-	ClearVertex() : x(0.0f), y(0.0f) {};
-	ClearVertex(float x, float y) : x(x), y(y) {};
+	Vector2f()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+	Vector2f(float inX, float inY)
+	{
+		x = inX;
+		y = inY;
+	}
 	float x, y;
-};
-
-struct UnlitColorVertex {
-	UnlitColorVertex() : x(0.0f), y(0.0f), z(0.0f), col() {};
-	UnlitColorVertex(float x, float y, float z, Color color) : x(x), y(y), z(z), col(color) {};
-	float x, y, z;
-	Color col;
 };
 
 struct Vector3f
@@ -37,12 +31,52 @@ struct Vector3f
 		y = inY;
 		z = inZ;
 	}
-	//union { float x; float r; };
+	//union { float x; float u; };
 	//union { float y; float g; };
 	//union { float z; float b; };
 	float x, y, z;
 };
 
+struct Color
+{
+	Color() : r(0.0f), g(0.0f), b(0.0f), a(0.0f) {};
+	Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {};
+	float r, g, b, a;
+};
+
+struct ClearVertex
+{
+	ClearVertex() : x(0.0f), y(0.0f) {};
+	ClearVertex(float x, float y) : x(x), y(y) {};
+	float x, y;
+};
+
+struct UnlitColorVertex
+{
+	UnlitColorVertex() : x(0.0f), y(0.0f), z(0.0f), col() {};
+	UnlitColorVertex(float x, float y, float z, Color color) : x(x), y(y), z(z), col(color) {};
+	float x, y, z;
+	Color col;
+};
+
+struct UnlitTexturedVertex
+{
+	UnlitTexturedVertex() : x(0.0f), y(0.0f), z(0.0f), uv(0.0f, 0.0f) {};
+	UnlitTexturedVertex(float x, float y, float z, float u, float v) : x(x), y(y), z(z), uv(u, v) {};
+	UnlitTexturedVertex(float x, float y, float z, Vector2f uv) : x(x), y(y), z(z), uv(uv) {};
+	float x, y, z;
+	Vector2f uv;
+};
+
+struct TexturedVertex
+{
+	TexturedVertex() : x(0.0f), y(0.0f), z(0.0f), uv(0.0f, 0.0f), normal(0.0f, 0.0f, 0.0f) {};
+	TexturedVertex(float x, float y, float z, float u, float v, float nx, float ny, float nz) : x(x), y(y), z(z), uv(u, v), normal(nx, ny, nz) {};
+	TexturedVertex(float x, float y, float z, Vector2f uv, Vector3f normal) : x(x), y(y), z(z), uv(uv), normal(normal) {};
+	float x, y, z;
+	Vector2f uv;
+	Vector3f normal;
+};
 
 inline Vector3f operator+(const Vector3f& left, const Vector3f& right)
 {
