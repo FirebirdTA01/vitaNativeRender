795c9d1fb6d751fd5afaf7ddbdbe573585e0cd8b
FirebirdTA01 <FirebirdTA01@aol.com>
2025-08-02 22:09:53 -0500

    Add PSVITA platform and fix toolchain include paths

---
+++ Platform/PSVITA.cmake
@@ -0,0 +1,6 @@
+# Minimal platform definition for PlayStation Vita
+# Presence of this file allows CMake to recognise PSVITA as a valid system.
+
+set(CMAKE_SYSTEM_NAME PSVITA)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
